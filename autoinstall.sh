f72047e5b527ee670e0424faaae133b50edadba6
Kevin Diale <powersurge360@gmail.com>
2021-08-06 15:38:44 -0400

    Added a hack for work gitconfig

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,6 +4,7 @@ mkdir ~/.config
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/ackrc ~/.ackrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/work.gitconfig ~/.work.gitconfig
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
