88b283406fb67f4b66d6ddee3f9f647e2c18e1c4
Kevin Diale <kevin.diale@drizly.com>
2022-07-25 16:48:49 -0400

    More updates

--- doom.d/config.el
+++ doom.d/config.el
@@ -38,6 +38,8 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(add-to-list 'default-frame-alist '(alpha 85))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
