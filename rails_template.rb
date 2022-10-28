912221ab5eaff0fee52d94fa02116cc0c494b398
Kevin Diale <powersurge360@gmail.com>
2022-10-27 22:13:20 -0400

    Moved where lookbook is set up

--- rails_template.rb
+++ rails_template.rb
@@ -1,15 +1,19 @@
 gem "view_component"
-gem "lookbook"
 gem "cssbundling-rails"
 gem "vite_rails"
 
 gem_group :development, :test  do
   gem "rspec-rails"
+  gem "lookbook"
 end
 
 gem_group :development do
   gem "rubocop-shopify", require: false
   gem "solargraph", require: false
+end
+
+environment do
+  "config.view_component.generate.preview = true"
 end
 
 run "bundle install"
