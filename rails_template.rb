c0010715b04221433f4775a1da64cf108a576d9c
Kevin Diale <powersurge360@gmail.com>
2023-06-16 16:19:06 -0400

    Updated rails template

--- rails_template.rb
+++ rails_template.rb
@@ -1,3 +1,10 @@
+def natively_supports_docker?
+  docker_supported_version = Gem::Version.new("7.1")
+  this_rails_version = Gem::Version.new(Rails.version) 
+
+  this_rails_version > docker_supported_version
+end
+
 gem "view_component"
 
 gem_group :development, :test  do
@@ -12,14 +19,16 @@ gem_group :development do
   gem "solargraph", require: false
   gem "brakeman"
   gem "bundle-audit"
+  gem "dockerfile-rails" unless natively_supports_docker?
 end
 
 environment "config.view_component.generate.preview = true"
-environment <<EOL
+environment <<EOL,
 config.lookbook.preview_display_options = {
     wrapper: true
 }
 EOL
+  env: "development"
 
 run "bundle install"
 
@@ -28,6 +37,7 @@ generate "rspec:install"
 run "bundle binstubs brakeman"
 run "bundle binstubs bundler-audit"
 run "bundle binstubs rubocop"
+run "bundle binstubs rspec-core"
 
 rubocop_config = <<-EOL
 inherit_gem:
@@ -46,3 +56,18 @@ create_file ".rubocop.yml", rubocop_config
 
 # Run rubocop against default files to start off with everything fixed
 run "rubocop -A ."
+
+esbuild_config = <<-EOL
+{
+  "compilerOptions": {
+    "paths": {
+      "components/*": ["./app/components/*"]
+    }
+  },
+  "include": []
+}
+EOL
+
+create_file "tsconfig.json", esbuild_config
+
+generate "dockerfile"
