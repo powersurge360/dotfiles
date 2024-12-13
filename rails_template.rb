c33948e80bcf48df3cd30dbae52d527f1a391854
Kevin Diale <powersurge360@gmail.com>
2024-12-13 14:04:04 -0500

    Tmuxp support for rails templates

--- rails_template.rb
+++ rails_template.rb
@@ -1,9 +1,10 @@
+template "~/dotfiles/rails.tmuxp.yaml.tt", "~/.tmuxp/#{@app_name}.yaml"
+
 gem_group :development, :test do
-  gem 'rspec-rails'
+  gem 'rspec-rails', git: "https://github.com/rspec/rspec-rails"
   gem 'factory_bot_rails'
 end
 
-# Needs to be in the after bundle to make sure all dependencies are captured, unfortunately
 after_bundle do
   generate 'rspec:install'
   run 'bundle binstubs rspec-core'
