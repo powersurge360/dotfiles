33c72253ca473fd03720572ac987661c17c4c256
Kevin Diale <powersurge360@gmail.com>
2025-02-21 17:15:02 -0500

    Lazyvim corrections

---
+++ lazyvim-plugins/nvim-lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				ruby_lsp = {
+					mason = false,
+					cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+				},
+				rubocop = {
+					mason = false,
+					cmd = { vim.fn.expand("~/.asdf/shims/rubocop") },
+				},
+			},
+		},
+	},
+}
